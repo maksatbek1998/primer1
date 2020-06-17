9cdc85d8c75efa0cdcb39856d948c40880d8db32
maksatbek1998 <maksat.140298.kg@gmail.com>
2020-06-17 19:16:08 +0600

    dddd

--- Maksat_Project/Program.cs
+++ Maksat_Project/Program.cs
@@ -10,10 +10,10 @@ namespace Maksat_Project
     {
         static void Main(string[] args)
         {
+            //Maksat kandayффффффффfhgfh
             //Maksat kandayфффффффф
             //Maksat kandayфффффффф
-            //Maksat kandayфффффффф
-            //Maksat kandayффффффффSDvdfsvfbv
+            //Maksat kandayффффффффSDvdfsvfbvgfhf
             Console.ReadKey();
         }
     }
